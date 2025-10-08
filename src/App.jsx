ceed07d7950643a6b43e07ba6e27abea895cb90f
Aniket <aniketgupta6394@gmail.com>
2025-10-08 11:13:04 +0530

    user friendly UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,40 @@
 import React from "react";
-import {
-  Container,
-  Typography,
-  Paper,
-  Box,
-} from "@mui/material";
+import { Container, Typography, Paper, Box } from "@mui/material";
 import TodoForm from "./components/toDoForm";
 import TodoList from "./components/toDoList";
+import "./App.css"
 
 function App() {
   return (
-    <Container maxWidth="sm" sx={{ minHeight: '100vh', bgcolor: '#f5f5f5', py: 5 }}>
-      <Paper
-        elevation={4}
-        sx={{
-          borderRadius: 4,
-          padding: 4,
-          boxShadow: "0 8px 16px rgba(0,0,0,0.1)",
-        }}
+    <div className="animated-bg">
+      <Container
+        maxWidth="sm"
+        sx={{ minHeight: "100vh", bgcolor: "#f5f5f561", py: 5 }}
       >
-        <Box textAlign="center" mb={3}>
-          <Typography variant="h4" color="primary" gutterBottom>
-            🎯 Redux To-Do
-          </Typography>
-          <Typography variant="subtitle1" color="textSecondary">
-            Stay organized and boost productivity
-          </Typography>
-        </Box>
-        <TodoForm />
-        <Box mt={4}>
-          <TodoList />
-        </Box>
-      </Paper>
-    </Container>
+        <Paper
+          elevation={4}
+          sx={{
+            borderRadius: 4,
+            marginTop:6,
+            padding: 4,
+            boxShadow: "0 8px 16px rgba(0,0,0,0.1)",
+          }}
+        >
+          <Box textAlign="center" mb={3}>
+            <Typography variant="h4" color="primary" gutterBottom>
+              🎯 Redux To-Do
+            </Typography>
+            <Typography variant="subtitle1" color="textSecondary">
+              Stay organized and boost productivity
+            </Typography>
+          </Box>
+          <TodoForm />
+          <Box mt={4}>
+            <TodoList />
+          </Box>
+        </Paper>
+      </Container>
+    </div>
   );
 }
 
